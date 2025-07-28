88c21a07755eae46cfc8a25d805fe97d7691e8c7
SRPater <srpater@gmail.com>
2025-07-28 16:35:02 +0200

    Complete CH1: LLMs, L6: Verbose

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    args = sys.argv[1:]
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1]:
+        if not arg.startswith("--"):
+            args.append(arg)
 
     if not args:
         print("AI Code Assistant")
@@ -19,20 +23,26 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    user_prompt = " ".join(args)
+    user_prompt = " ".join(args[:-1])
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    generate_content(client, messages)
+    generate_content(client, messages, verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
