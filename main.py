74a54b9150d8557663a9cfb530d58cec54894afb
SRPater <srpater@gmail.com>
2025-07-29 11:15:30 +0200

    Complete CH3: Function Calling, L1: System Prompt

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 
+from prompts import system_prompt
+
 
 def main():
     load_dotenv()
 
     verbose = "--verbose" in sys.argv
     args = []
-    for arg in sys.argv[1]:
+    for arg in sys.argv[1:]:
         if not arg.startswith("--"):
             args.append(arg)
 
     if not args:
         print("AI Code Assistant")
         print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -39,6 +41,7 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
